506b8a4c8cb6c28b3ecc5270a7f86844aa45e1f8
JorgeAndresCastillo <jorgeandresprogramador@gmail.com>
2020-10-15 17:52:49 -0500

    definiendo colores y redondiando botones

---
+++ app/src/main/java/com/nojoda/viajaseguro/ParqueCafeActivity.java
@@ -0,0 +1,14 @@
+package com.nojoda.viajaseguro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ParqueCafeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parque_cafe);
+    }
+}
